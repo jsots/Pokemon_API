5853a326f42b16de461a3239b59598013d81c661
jsots <justinsotolongo@gmail.com>
2023-04-11 14:00:12 -0400

    MVP, want to add images

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
 import chalk from "chalk"
 
-const MONGODB_URI = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/pokemon-api" // if the first thing doesnt exist, it will defualt to the second thing which is what we normally do
+const url = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/pokemon-api" // if the first thing doesnt exist, it will defualt to the second thing which is what we normally do
 
 // This is for Model.findByIdAndUpdate method, specifically so that {new: true}
 mongoose.set("returnOriginal", false)
 
-mongoose.connect(MONGODB_URI).catch((error) => console.log("Error connecting to MongoDB: ", error.message))
+mongoose.connect(url).catch((error) => console.log("Error connecting to MongoDB: ", error.message))
 
 mongoose.connection.on("disconnected", () => console.log(chalk.bold("Disconnected from MongoDB!")))
 
